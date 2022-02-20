9d8c5bd54a845e8c214575bfcc251a03605d2671
git init <butkepavan55@gmail.com>
2022-02-20 21:27:09 +0530

    added

--- client/src/components/Upload_sec.jsx
+++ client/src/components/Upload_sec.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types';
-import StudentRow from './StudentRow';
+
 import { Typography } from "@material-ui/core";
 
 import UploadImages from "../services.js/upload-file.service";
@@ -8,18 +7,12 @@ import './Student.css'
 
 function upload_sec() {
   return (
-    <div className="container">
-      <div className="mg20">
-        <Typography variant="h5">Upload</Typography>
-        <Typography variant="h6">Material UI Image Upload with Preview</Typography>
-      </div>  <UploadImages  />
+    <div>
+        <h1></h1>
     </div>
-    
+
 
   )
 }
-StudentRow.propTypes = {
-    student: PropTypes.object.isRequired
-  };
 
 export default upload_sec;
