73a7fee75434a8d30866311765c3d35c9ee1772e
git init <butkepavan55@gmail.com>
2022-02-20 18:11:47 +0530

    added server.js

--- src/components/StudentRow.jsx
+++ src/components/StudentRow.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function StudentRow() {
-  return (
-    <div>StudentRow</div>
-  )
+class StudentRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { student, rank, sort } = this.props
+    return (
+    
+        <tr>
+        <td >{sort ? rank+1 : ''}</td>
+        <td a>{student.name}</td>
+        <td>{student.marks1}</td>
+        <td>{student.marks2}</td>
+        <td>{student.marks3}</td>
+      </tr>
+
+    );
+  }
 }
 
-export default StudentRow
+StudentRow.propTypes = {
+  student: PropTypes.object.isRequired
+};
+
+export default StudentRow;
