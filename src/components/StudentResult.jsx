c826082c94376510118d11bc50e87bbc32d069d1
git init <butkepavan55@gmail.com>
2022-02-20 17:38:22 +0530

    data updated

--- src/components/StudentResult.jsx
+++ src/components/StudentResult.jsx
@@ -7,16 +7,21 @@ class StudentRow extends React.Component {
     this.state = {};
   }
   render() {
-
+    const { student, rank, sort } = this.props
     return (
     
-        
+        <tr>
+        <td >{sort ? rank+1 : ''}</td>
+        <td >{student.name}</td>
+        <td>{student.marks1}</td>
+        <td>{student.marks2}</td>
+        <td>{student.marks3}</td>
+      </tr>
+
     );
   }
 }
 
-StudentRow.propTypes = {
-  student: PropTypes.object.isRequired
-};
+
 
 export default StudentRow;
